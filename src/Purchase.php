651b0ff47746f665eabb57c773290ebd721aa77c
Jun Martin <jun.martin.setiady@gmail.com>
2016-10-17 20:23:14 +0700

    Purchase Class

---
+++ src/Purchase.php
@@ -0,0 +1,15 @@
+<?php
+
+Namespace Junmartin\Test_lvl;
+class Purchase{
+	protected $doc_no;
+
+	public function __construct($value='')
+	{
+		$this->doc_no = $value;
+	}
+	public function get_doc_no()
+	{
+		return $this->$doc_no."_123";
+	}
+}
